d7f346187d7bef054bb2ca0f3a5ff694276eb459
fatima-elfat <ffzelfatehy@gmail.com>
2023-05-30 17:28:19 -0700

    printf part 2

---
+++ files/main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry function
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+    _puts("-----main----");
+    /*_printf("trinyg to print %li.", 12345678);*/
+    _printf("trinyg to print %*i \n", 5, 20);
+    _printf("Integers: %+i  \n", 3456);
+    _printf("Integers: %+u \n", 3456);
+    _printf("Some different radices: %o \n", 100);
+    _printf("Some different radices: %x \n", 100);
+    _printf("Some different radices: %X \n", 100);
+    _printf("this is %R \n", "TRRXF SBE TRRXF");
+    _printf("this is %r \n", "TRRXF SBE TRRXF");
+    _printf("this is %s \n", "GEEKS FOR GEEKS");
+    _printf("this is %% \n");
+    _printf("trinyg to print %d %d \n", 5, 20);
+    _printf("trinyg to print %*i \n", 5, 20);
+    _printf("trinyg to print %.*i \n", 5, 20);
+    _printf("trinyg to print %-5i \n", 20);
+    /*
+    _printf("trinyg to print %i, %d, %i, %d.", -1000, 200, 40, 2);
+    */
+    return (0);
+}
---
+++ utils.c
@@ -0,0 +1,78 @@
+#include "main.h"
+/**
+ * _longintlen - returns the length of a int.
+ * @i: input string
+ * Return: length
+ */
+
+/**
+ * _intlen - returns the length of a int.
+ * @i: input string
+ * Return: length
+ */
+unsigned int _intlen(int i)
+{
+    int l = 0;
+	if (i == 0)
+		l = 1;
+    if (i < 0)
+    {
+        l++;
+    }
+    while(i != 0)
+    {
+        i /= 10;
+        l++;
+    }
+    return (l);
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: input string
+ * Return: length
+ */
+unsigned int _strlen (char *s)
+{
+	if (*s == '\0' || *s == '\n' || *s == 0)
+		return (0);
+	s++;
+	return (1 + _strlen(s));
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+/**
+ * _puts - prints a string followed by a new line
+ * @s: string to print.
+ * Return: length of string.
+ */
+int _puts(char *s)
+{
+	while (*s != '\0')
+		_putchar(*s++);
+
+	_putchar('\n');
+	return (_strlen(s));
+}
+/**
+ * _puts_buffer - prints a buffer.
+ * @s: buffer to print.
+ * @l: length of buffer.
+ * Return: length of string.
+ */
+int _puts_buffer(char *s, unsigned int l)
+{
+    unsigned int i = 0;
+	while (i < l)
+    {
+        _putchar(*s++);
+        i++;
+    }
+	return (l);
+}
